d714577bb9747fe24966b64df884dfe652df5cf7
lichanghong <2754237316@qq.com>
2022-03-18 21:37:48 +0800

    diamante抽取

--- manage.py
+++ manage.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from info import create_app, db
 
-app = Flask(__name__)
+app = create_app('development')
+
+# Flask-script
+manager = Manager(app)
+# 数据库迁移
+Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
 
 @app.route('/index')
 def index():
     return 'index'
 
+@app.route('/')
+def helloworld():
+    return 'helloworld'
+
+
 if __name__ == '__main__':
-    app.run()
+    manager.run()
